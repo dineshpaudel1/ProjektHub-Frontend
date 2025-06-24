abea5d0128fa882ab281ab2a958e6654c5f2c7a0
Dinesh Paudel <paudeldinesh961@gmail.com>
2025-06-24 22:16:24 +0545

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AdminLayout from "./layouts/AdminLayout";
